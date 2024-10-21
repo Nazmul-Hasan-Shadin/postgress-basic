8492494c29f5e1733975d142576d5a72b224d841
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-10-21 20:52:15 +0600

    group by query has added

---
+++ groupBy.sql
@@ -0,0 +1,9 @@
+SELECT * from students;
+
+SELECT country, count(*),avg(age) from  students GROUP BY country;
+
+ 
+--  Filter groups using having to show only countries with average age above 22
+SELECT country,avg(age) from  students GROUP BY country HAVING avg(age) >20;
+
+ select extract(year from dob) as birth_year ,count(*)  from students GROUP BY birth_year
