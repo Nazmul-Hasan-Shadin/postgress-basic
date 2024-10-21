d8afa206d5536211181cf9e9b43927c5d0a9777e
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-10-21 14:26:31 +0600

    more qury has added

--- select.sql
+++ select.sql
@@ -33,4 +33,95 @@ VALUES
 
 -- for sorting 
 
---   SELECT * from students ORDER BY first_name DESC  ;
+  SELECT * from students ORDER BY first_name DESC  ;
+
+
+-- for find unique country from table where student get admission we use distinict
+
+select DISTINCT country from students ORDER BY country asc;
+
+-- find data by filter
+
+select * from students WHERE country='Brazil';
+
+SELECT * from students WHERE grade='B' AND course='Psychology'
+
+SELECT * FROM students WHERE (country='Brazil' OR country='India' )and age=21;
+
+SELECT * FROM students WHERE age >20 and course='History'
+
+SELECT * FROM students WHERE age !=23
+
+-- to make capital leter of first letter it make anoter collom naem upper
+
+-- scalaer fucntuion  run for individual row
+/*
+
+ upper()
+ lower()
+ concat()
+ length()
+
+
+*/
+SELECT upper(first_name),  * from students
+
+SELECT concat(first_name , ' ' , last_name) from students;
+
+
+--     Aggrigiate function
+
+/*
+  avg() calculate the average of a set of values,
+  maxx() return the max value in a set 
+  min() returns the minimum value in a set. 
+  sum() calculate the sum of values in a set .
+  count () counts the number of rows in a set
+
+
+
+*/
+
+
+SELECT avg(age) from students ;
+select count(*) from students;   age
+
+
+select max(length(first_name)) from students;
+
+-- bellow expression wont work beacaue perform + - anything with null retrun resutl null . we need a boolean for succesful query
+
+select * from students where email !=NULL
+
+-- for avobe problem
+SELECT * from students WHERE email IS null;
+
+
+SELECT email from students;
+
+-- if a columen have null vaule and during query i want to show instedad of null onters we can use here coalesce();
+
+SELECT COALESCE(email,'Email is not provided') as "Email",first_name,blood_group from students
+
+
+-- 
+SELECT * from students WHERE country='USA' OR country='Canada' or country='UK'
+-- abobe query is too big to make smaller write in another pg_get_wal_resource_managers
+
+SELECT * from students WHERE country NOT IN('USA','UK');
+-- use between poerator
+SELECT  * FROM students WHERE age BETWEEN 19 and 22 ORDER BY age;
+ age
+--  like e operatr its case SENSITIVE for search
+
+SELECT *  from students WHERE first_name LIKE '%an'
+
+SELECT *  from students WHERE first_name LIKE '__a%'
+ 
+
+
+DELETE from students WHERE grade='B'
+
+-- update a column data
+
+UPDATE students SET emai='default@gmal.com' WHERE  student_id='1'
